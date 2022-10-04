cd15566dc5ba35e7151b914322763eaa34ae616d
usuario <usuario@pc21-086>
2022-10-04 17:00:00 +0200

    Saludo al usuario

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,6 +3,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!!!");
+		String name = "Juanma";
+		System.out.println("Bienvenido, " + name);
 	}
 
 }
